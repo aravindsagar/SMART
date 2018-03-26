486919d7cfa24f7dd2ae14579c095aa9d72217ea
Aravind Sagar <aravindsuhruth@gmail.com>
2018-03-26 12:04:54 -0500

    The report top bar is ready

--- SMART/app/src/main/java/com/cs565project/smart/MainActivity.java
+++ SMART/app/src/main/java/com/cs565project/smart/MainActivity.java
@@ -1,12 +1,11 @@
 package com.cs565project.smart;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
--- SMART/app/src/main/java/com/cs565project/smart/fragments/DatePickerFragment.java
+++ SMART/app/src/main/java/com/cs565project/smart/fragments/DatePickerFragment.java
@@ -2,28 +2,41 @@ package com.cs565project.smart.fragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.widget.Toast;
 
 import com.cs565project.smart.R;
 import com.savvi.rangedatepicker.CalendarPickerView;
 
 import java.util.Date;
+import java.util.List;
 
 import static com.savvi.rangedatepicker.CalendarPickerView.SelectionMode.RANGE;
 import static com.savvi.rangedatepicker.CalendarPickerView.SelectionMode.SINGLE;
 
 /**
- * A dialog box for picking a date or a date range.
+ * A dialog box for picking a date or a date range. Activities using this Dialog should implement
+ * OnDateSelectedListener.
  */
 
 public class DatePickerFragment extends DialogFragment implements DialogInterface.OnClickListener {
+
+    /**
+     * Callback interface to pass the selected date values back to the parent activity.
+     */
+    public interface OnDateSelectedListener {
+        void onDateSelected(List<Date> selectedDates);
+    }
+
     private static final String KEY_SELECT_RANGE = "SELECT_RANGE";
     private static final String KEY_START_DATE   = "START_DATE";
     private static final String KEY_END_DATE     = "END_DATE";
+
+    private OnDateSelectedListener myListener;
+    private CalendarPickerView myPicker;
 
     public static DatePickerFragment getInstance(boolean selectRange, long startDate, long endDate) {
         DatePickerFragment fragment = new DatePickerFragment();
@@ -34,6 +47,23 @@ public class DatePickerFragment extends DialogFragment implements DialogInterfac
         fragment.setArguments(bundle);
         return fragment;
     }
+
+    public DatePickerFragment setListener(OnDateSelectedListener listener) {
+        myListener = listener;
+        return this;
+    }
+
+    /*@Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // Make sure that the container of this Fragment has implemented the callback interface.
+        try {
+            myListener = (OnDateSelectedListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement NoticeDialogListener");
+        }
+    }*/
 
     @NonNull
     @Override
@@ -48,12 +78,12 @@ public class DatePickerFragment extends DialogFragment implements DialogInterfac
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
 
-        CalendarPickerView datePickerView = (CalendarPickerView)
-                getLayoutInflater().inflate(R.layout.date_range_picker, null);
-        datePickerView.init(new Date(args.getLong(KEY_START_DATE)), new Date(args.getLong(KEY_END_DATE)))
+        myPicker = (CalendarPickerView)
+                getActivity().getLayoutInflater().inflate(R.layout.date_range_picker, null);
+        myPicker.init(new Date(args.getLong(KEY_START_DATE)), new Date(args.getLong(KEY_END_DATE)))
                 .inMode(args.getBoolean(KEY_SELECT_RANGE) ? RANGE : SINGLE);
 
-        builder.setView(datePickerView)
+        builder.setView(myPicker)
                 .setPositiveButton("OK", this)
                 .setCancelable(true);
         return builder.create();
@@ -61,6 +91,8 @@ public class DatePickerFragment extends DialogFragment implements DialogInterfac
 
     @Override
     public void onClick(DialogInterface dialog, int which) {
-        Toast.makeText(getContext(), "Good", Toast.LENGTH_SHORT).show();
+        if (myListener != null) {
+            myListener.onDateSelected(myPicker.getSelectedDates());
+        }
     }
 }
