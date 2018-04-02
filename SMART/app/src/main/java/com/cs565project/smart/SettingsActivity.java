80db0141d0b4d6f384277e6e4aefb5f25ecaefb5
Aravind Sagar <aravindsuhruth@gmail.com>
2018-04-02 00:48:11 -0500

    Add persistent notification so that the service won't be stopped by Android system.

--- SMART/app/src/main/java/com/cs565project/smart/SettingsActivity.java
+++ SMART/app/src/main/java/com/cs565project/smart/SettingsActivity.java
@@ -1,15 +1,14 @@
 package com.cs565project.smart;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Pair;
 
 import com.cs565project.smart.fragments.GeneralSettingsFragment;
@@ -19,21 +18,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,14 +31,14 @@ public class SettingsActivity extends AppCompatActivity {
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        ViewPager viewPager = findViewById(R.id.container);
+        viewPager.setAdapter(sectionsPagerAdapter);
 
         TabLayout tabLayout = findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     /**
--- SMART/app/src/main/java/com/cs565project/smart/service/AppMonitorService.java
+++ SMART/app/src/main/java/com/cs565project/smart/service/AppMonitorService.java
@@ -1,40 +1,62 @@
 package com.cs565project.smart.service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.WindowManager;
 
-import com.cs565project.smart.util.AppInfo;
+import com.cs565project.smart.MainActivity;
+import com.cs565project.smart.R;
 import com.cs565project.smart.util.UsageStatsUtil;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class AppMonitorService extends Service {
+
+    private static final String CHANNEL_ID = "persistent";
+
     public static final String ACTION_START_SERVICE = "start_service";
     public static final String ACTION_STOP_SERVICE = "stop_service";
     public static final String ACTION_TOGGLE_SERVICE = "toggle_service";
 
+    private static final int CYCLE_DELAY = 200;
+    private static final int DATA_UPDATE_DELAY = 60000;
+
     private UsageStatsUtil myUsageStatsUtil;
     private BlockOverlay myOverlay;
     private boolean isRunning = false;
+    private String notificationTitle = "SMART is starting up";
+    private String notificationText = "You day summary will appear here";
+    private boolean updateNotification = false;
+    private int cycleCount = DATA_UPDATE_DELAY/CYCLE_DELAY - 1;
     private Executor myExecutor = Executors.newSingleThreadExecutor();
     private Handler myHandler = new Handler();
     private Runnable myBgJobStarter = new Runnable() {
         @Override
         public void run() {
             myExecutor.execute(myBgJob);
+            if (updateNotification) {
+                postNotification(notificationTitle, notificationText);
+                updateNotification = false;
+            }
         }
     };
 
     private Runnable myBgJob = new Runnable() {
         @Override
         public void run() {
+            cycleCount++;
             String currentApp = myUsageStatsUtil.getForegroundApp();
-            Log.d("CURRENT_APP", currentApp + "");
+            // Log.d("CURRENT_APP", currentApp + "");
 
             // Adding/removing overlay should happen in the main thread.
             if ("com.google.android.apps.messaging".equals(currentApp)) {
@@ -44,9 +66,18 @@ public class AppMonitorService extends Service {
                 myHandler.post(myHideOverlay);
             }
 
+            if (cycleCount == DATA_UPDATE_DELAY/CYCLE_DELAY) {
+                cycleCount = 0;
+                Log.d("SMART", "Updating app usage data");
+                // TODO collect app usage time and update db.
+                // TODO update notification title and text.
+                notificationText = "1:20 hours in restricted apps";
+                notificationTitle = "Good job!";
+                updateNotification = true;
+            }
             // Schedule the app check to run again after a second.
             if (isRunning) {
-                myHandler.postDelayed(myBgJobStarter, 1000);
+                myHandler.postDelayed(myBgJobStarter, CYCLE_DELAY);
             }
         }
     };
@@ -106,5 +137,31 @@ public class AppMonitorService extends Service {
 
         isRunning = true;
         myHandler.post(myBgJobStarter);
+    }
+
+    private void postNotification(String title, String text){
+        createNotificationChannel();
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        Intent intent = new Intent(this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pi = PendingIntent.getActivity(this, 2, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification n = notificationBuilder.setContentTitle(title).
+                setSmallIcon(R.mipmap.ic_launcher).setContentText(text).setPriority(NotificationCompat.PRIORITY_MIN).
+                setContentIntent(pi).build();
+        startForeground(1, n);
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_MIN);
+            channel.setDescription(description);
+            // Register the channel with the system
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
