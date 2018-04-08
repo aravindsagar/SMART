fffbfdb206889026374b429f62f1cbb7a5237c6d
LDH <ldh4.ch@gmail.com>
2018-04-08 17:33:35 -0500

    Simple examples of Emotion and Vision API

---
+++ SMART/app/src/main/java/emotion/Emotion.java
@@ -0,0 +1,55 @@
+package emotion;
+
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class Emotion
+{
+    public static void main(String[] args)
+    {
+        HttpClient httpClient = new DefaultHttpClient();
+
+        try
+        {
+            // NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
+            //   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the
+            //   URL below with "westcentralus".
+            URIBuilder uriBuilder = new URIBuilder("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize");
+
+            URI uri = uriBuilder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers. Replace the example key below with your valid subscription key.
+            request.setHeader("Content-Type", "application/json");
+
+            // FIXME: Add in name and subscription key when acquired
+            request.setHeader(" ", " ");
+
+            // Request body. Replace the example URL below with the URL of the image you want to analyze.
+            // FIXME: the image will have to be stored on cloud (Azure or somewhere accessible by Azure Emotion
+            StringEntity reqEntity = new StringEntity("{ \"url\": \"http://example.com/images/test.jpg\" }");
+            request.setEntity(reqEntity);
+
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null)
+            {
+                // Sample response would be in JSON with facial location within the provided image
+                // and scores of each facial expression categories
+                System.out.println(EntityUtils.toString(entity));
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
